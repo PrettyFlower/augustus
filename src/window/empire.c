d2fc3a4b134b2a97a7d34a901efe8ded5ddb2a24
PrettyFlower <abigchode@gmail.com>
2022-11-12 22:32:07 -0500

    Add new trade amount sprites to better show the high values available in custom empires.

--- src/window/empire.c
+++ src/window/empire.c
@@ -32,9 +32,22 @@
 #include "window/trade_prices.h"
 
 #include <math.h>
+#include <stdlib.h>
 
 #define MAX_WIDTH 2032
 #define MAX_HEIGHT 1136
+
+typedef struct {
+    int x;
+    int y;
+} px_point;
+static px_point trade_amount_px_offsets[5] = {
+    { 2, 0 },
+    { 5, 2 },
+    { 8, 4 },
+    { 0, 3 },
+    { 4, 6 },
+};
 
 static void button_help(int param1, int param2);
 static void button_return_to_city(int param1, int param2);
@@ -147,12 +160,29 @@ static void draw_trade_resource(resource_type resource, int trade_max, int x_off
         button_border_draw(x_offset - 2, y_offset - 2, 101 + 4, 30, 1);
     }
 
-    if (trade_max < 20) {
-        image_draw(image_group(GROUP_TRADE_AMOUNT), x_offset + 21, y_offset - 1, COLOR_MASK_NONE, SCALE_NONE);
-    } else if (trade_max < 32) {
-        image_draw(image_group(GROUP_TRADE_AMOUNT) + 1, x_offset + 17, y_offset - 1, COLOR_MASK_NONE, SCALE_NONE);
+    window_empire_draw_resource_shields(trade_max, x_offset, y_offset);
+}
+
+void window_empire_draw_resource_shields(int trade_max, int x_offset, int y_offset)
+{
+    int img_offset = min(trade_max / 100, 4);
+    int num_shields = (trade_max % 100) / 20 + 1;
+    if (trade_max >= 600) {
+        num_shields = 5;
+    }
+    int shield_image_id = assets_lookup_image_id(ASSET_TRADE_AMOUNT) + img_offset;
+    int top_left_x;
+    if (num_shields == 1) {
+        top_left_x = x_offset + 19;
+    } else if (num_shields == 2) {
+        top_left_x = x_offset + 15;
     } else {
-        image_draw(image_group(GROUP_TRADE_AMOUNT) + 2, x_offset + 13, y_offset - 1, COLOR_MASK_NONE, SCALE_NONE);
+        top_left_x = x_offset + 11;
+    }
+    int top_left_y = y_offset - 1;
+    for (int i = 0; i < num_shields; i++) {
+        px_point pt = trade_amount_px_offsets[i];
+        image_draw(shield_image_id, top_left_x + pt.x, top_left_y + pt.y, COLOR_MASK_NONE, SCALE_NONE);
     }
 }
 
